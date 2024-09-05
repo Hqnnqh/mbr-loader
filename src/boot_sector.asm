38ce9b0d50dab5e5e410a1a11e4601135658a29c
Hqnnqh <hannah@diefluchs.at>
2024-09-05 19:25:21 +0200

    Implement basic text display

--- src/boot_sector.asm
+++ src/boot_sector.asm
@@ -1,9 +1,28 @@
 ;
-; A simple boot sector program that loops forever.
+; A simple boot sector program that prints a message to the screen using a BIOS routine.
 ;
 bits 16 ; 16-bit real mode
-loop:
-    jmp loop ; loop forever
+
+; put BIOS routine identifier in high half of ax and character to print in low half.
+
+mov ah, 0x0e ; int 10/ah is the scrolling teletype BIOS routine
+
+mov al, 'H'
+int 0x10 ; call interrupt for BIOS routines
+
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'o'
+int 0x10
+
+jmp $ ; loop forever
 
 times 510-($-$$) db 0 ; fill entire binary except for last 2 bytes with 0 (as padding)
 
