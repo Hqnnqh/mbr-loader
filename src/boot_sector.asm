d10f5ac3628de0eaeb48addd47b462c98d2a81c4
Hqnnqh <hannah@diefluchs.at>
2024-09-09 21:33:56 +0200

    Implement print function

--- src/boot_sector.asm
+++ src/boot_sector.asm
@@ -4,29 +4,19 @@
 bits 16 ; 16-bit real mode
 [org 0x7c00] ; where BIOS should load bootloader code into memory
 
-%include "src/print.asm"
-
-; put BIOS routine identifier in high half of ax and character to print in low half.
-
-mov ah, 0x0e ; int 10/ah is the scrolling teletype BIOS routine
-
-mov al, 'H'
-int 0x10 ; call interrupt for BIOS routines
-
-mov al, 'e'
-int 0x10
-
-mov al, 'l'
-int 0x10
-
-mov al, 'l'
-int 0x10
-
-mov al, 'o'
-int 0x10
+mov bx, HELLO_MSG
+call print_string
 
 jmp $ ; loop forever
 
+; include
+%include "src/print.asm"
+
+; data
+HELLO_MSG :
+db 'Hello, World!', 0 ; zero makes the string null-terminaed
+
+; fill up sector binary
 times 510-($-$$) db 0 ; fill entire binary except for last 2 bytes with 0 (as padding)
 
 dw 0xaa55 ; Last two bytes form the magic number, so BIOS knows to load this program as the boot sector.
--- src/print.asm
+++ src/print.asm
@@ -3,5 +3,21 @@
 ;
 bits 16
 
-print:
-    nop
+print_string:
+    pusha
+    mov si, bx
+
+print_loop:
+    lodsb           ; load the byte at ds:si into al and increment si
+
+    cmp al, 0       ; end at null termination
+    je end_print    ; exit loop if end is reached
+
+    mov ah, 0x0e    ; int 10/ah is the scrolling teletype BIOS routine
+    int 0x10
+
+    jmp print_loop
+
+end_print:
+    popa
+    ret
